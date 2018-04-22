88371598fdc46d91fa6131fb5d4cdc799ea535c4
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-22 01:20:00 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      return "Your cart is empty" if @@cart.size == 0
+      resp.write "Your cart is empty" if @@cart.size == 0
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
